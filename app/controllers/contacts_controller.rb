b3f9778d592f54240021eee081308ac10b48026e
John Krone <john.krone@kellogg.northwestern.edu>
2023-05-18 22:45:39 +0000

    final commit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,12 +2,13 @@ class ContactsController < ApplicationController
 
   def show
     @contact = Contact.find_by({"id" => params["id"]})
-    @company = Company.find_by({"id" => @contact["company_id"]})
+    @company = Company.where({"id" => @contact["company_id"]})
     # render contacts/show view with details about Contact
   end
 
   def new
     @contact = Contact.new
+    @contact["company_id"] = params["company_id"]
     # render contacts/new view with new Contact form
   end
 
